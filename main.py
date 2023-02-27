99b6dd305b578ef76fc426ac44a5836dacc8fad0
lie1815 <lie1815@mail.ru>
2023-02-27 14:15:22 +0300

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+a = int(input("Введите значение\t"))
+b = int(input("Введите значение\t"))
+print((a-b)*(a+b))
+print(a^2-2^a*b+b^2)
+print(a^2+2*a*b+b^2)
+print(a^3-3*a^2*b+3*a*b^2-b^3)
+print(a^3+3*a^2*b+3*a*b^2+b^3)
+print((a+b)*(a^2-a*b+b^2))
+print((a-b)*(a^2+a*b+b^2))
+
+
+a = 100
+b = 10
+c = 6
+y1 = a*a
+y2 = a+c
+y3 = y2**2
+y4 = y1+y3
+y5 = 10*b
+y6 = y4/y5
+y7 = c**2
+y8 = a*y7
+y9 = 5*y8
+y10 = y6-y9
+y11 = y10++-1/2
+print(y11)
+
+
+
